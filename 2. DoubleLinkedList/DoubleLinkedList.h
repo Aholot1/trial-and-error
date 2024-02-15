587c0a2b48e7a327d7357425209eab15b8196abc
Aholot1 <ty090105@naver.com>
2024-02-15 13:47:42 +0900

    Update DoubleLinkedList.h

--- 2. DoubleLinkedList/DoubleLinkedList.h
+++ 2. DoubleLinkedList/DoubleLinkedList.h
@@ -14,6 +14,23 @@ typedef struct TagNode{
 Node* DLL_CreateNode(int DLL_NewData);
 void  DLL_DestroyNode(Node* Remove);
 
+void  DLL_AddNode(Node** Head, Node* NewNode);
+
+void  DLL_InsertAfterNode(Node* Current, Node* NewNode);
+void  DLL_InsertBeforeNode(Node* Current, Node* NewNode);
+
+void  DLL_RemoveNode(Node** Head, Node* Remove);
+void  DLL_CleanList(Node** Head);
+
+Node* DLL_SearchNode(Node** Head, int index);
+Node*   DLL_Top(Node** Head);
+Node*   DLL_Tail(Node** Head);
+
+void  DLL_PrintList(Node** Head);
+
+int   IsEmpty(Node** Head);
+int   IsFull(Node** Head);
+
 
 
 #endif 
