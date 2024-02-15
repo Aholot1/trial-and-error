b48e31e69eb1ffd0f9c59a35c6c08375858bff28
Aholot1 <ty090105@naver.com>
2024-02-15 13:33:22 +0900

    Update LinkedList.h

--- 1. LinkedList/LinkedList.h
+++ 1. LinkedList/LinkedList.h
@@ -17,14 +17,14 @@ void        SLL_DestroyNode(LinkedList* Node);
 
 void        SLL_NodeAdd(LinkedList** Head, LinkedList* NewNode);
 LinkedList* SLL_SearchNode(LinkedList** Head, int index);
-void        SLL_InsertAfterNode(LinkedList* Current, LinkedList *Add);
+void        SLL_InsertAfterNode(LinkedList* Current, LinkedList* Add);
 void        SLL_PrintList(LinkedList** Head);
 LinkedList* SLL_Top(LinkedList** Head);
 void        SLL_CleanList(LinkedList** Head);
 int         SLL_ReMoveNode(LinkedList** Head, LinkedList* Remove);
 int         SLL_IsEmpty(LinkedList** Head);
 int         SLL_IsFull(LinkedList** Head);
-void        SLL_InsertBeforeNode(LinkedList** Head, LinkedList* Current, LinkedList* Add);
+void        SLL_InsertBeforeNode(LinkedList** Head , LinkedList* Current, LinkedList* Add);
 
 
 #endif//asfasfasdffsafsaf
